2e79f4ef6cadcff974e49b2514e85a3bea09f6ed
salody <salodyliu@gmail.com>
2019-08-19 19:35:45 +0800

    后端开启keep-alive

--- app/web/service/article.js
+++ app/web/service/article.js
@@ -1,7 +1,19 @@
+import axios from 'axios'
 import request from '@/service/request'
 import API from '@/utils/api';
 
+
 export const getArticleDetail = articleId => {
   const url = API.getArticleDetail({ articleId });
-  return request.get({ url });
+  if (__IS_BROWSER) {
+    return request.get({ url });
+  } else {
+    const http = require('http')
+    const https = require('https')
+    return axios.get(url, {
+      timeout:    5000,
+      httpAgent:  new http.Agent({ keepAlive: true }),
+      httpsAgent: new https.Agent({ keepAlive: true }),
+    })
+  }
 };
