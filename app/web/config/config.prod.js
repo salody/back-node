9535327513fa5defea1c4c8e6d079e1c3c959226
salody <salodyliu@gmail.com>
2019-08-19 18:20:38 +0800

    h5课程测试

--- app/web/config/config.prod.js
+++ app/web/config/config.prod.js
@@ -7,9 +7,9 @@ module.exports = {
     fileName: '../../config/manifest.json',
   },
   define: {
-    'API.PLATFORM':           'https://dplatformapi.coolcollege.cn',
-    'API.ENTERPRISE':         'https://denterpriseapi.coolcollege.cn',
-    'API.HOST':               'https://disv.coolcollege.cn',
+    'API.PLATFORM':           'https://d2platformapi.coolcollege.cn',
+    'API.ENTERPRISE':         'https://d2enterpriseapi.coolcollege.cn',
+    'API.HOST':               'https://d2isv.coolcollege.cn',
     'GLOBAL.ENTERPRISE_HOST': 'https://dev.coolcollege.cn/mobile'
   }
 };
